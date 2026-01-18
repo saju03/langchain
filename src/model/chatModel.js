e2a29d803bf1e3cb50b60fb10f2cce8e8be1746d
saju03 <sajusuresh0752@gmail.com>
2026-01-19 01:29:31 +0530

    tools and agent creation

--- src/model/chatModel.js
+++ src/model/chatModel.js
@@ -6,7 +6,7 @@ import { getUserMemories } from "../app/tools.js";
 export const JSONModel = new ChatOllama({
   baseUrl: "http://127.0.0.1:11434", // 🔴 FORCE LOCALHOST
   model: "llama3.1",
-  temperature: 0.7,
+  temperature: 0,
 })
 
 export const chatModel = new ChatOllama({
